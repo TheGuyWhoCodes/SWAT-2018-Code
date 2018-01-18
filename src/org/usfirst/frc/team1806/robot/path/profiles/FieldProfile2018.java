af2ab194108910b5630b027baa4c9043f57c0dc7
Dillon Woollums <Dwooll94@gmail.com>
2018-01-18 14:07:34 -0600

    Made half field profile and corrected FieldProfile2018

--- src/org/usfirst/frc/team1806/robot/path/profiles/FieldProfile2018.java
+++ src/org/usfirst/frc/team1806/robot/path/profiles/FieldProfile2018.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1806.robot.path.profiles;
 
 /**
  * Interface that holds all the 2018 field measurements required by the PathAdapter
+ * An Implementing class should hold values that correspond to the delta between the measured distance and the official field distance.
  * 
  * @see PathAdapter
  */
@@ -21,5 +22,21 @@ public interface FieldProfile2018 {
 	
 	public double getRedWallToRightScale();
 	
-	public double getRedWallToRightScaleOuterEdge();
+	public double getRedCenterToRightScaleOuterEdge();
+	
+	public double getBlueWallToLeftSwitch();
+	
+	public double getBlueCenterToLeftSwitchOuterEdge();
+	
+	public double getBlueWallToRightSwitch();
+	
+	public double getBlueCenterToRightSwitchOuterEdge();
+	
+	public double getBlueWallToLeftScale();
+	
+	public double getBlueCenterToLeftScaleOuterEdge();
+	
+	public double getBlueWallToRightScale();
+	
+	public double getBlueCenterToRightScaleOuterEdge();
 }
