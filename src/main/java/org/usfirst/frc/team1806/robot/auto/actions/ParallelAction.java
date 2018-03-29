68325a06181645af788f06b97dbf6f9d67a38b56
TheGuyWhoCodes <christopherariagno@gmail.com>
2018-03-29 12:52:07 -0500

    Revert of Parallel Stuff

--- src/main/java/org/usfirst/frc/team1806/robot/auto/actions/ParallelAction.java
+++ src/main/java/org/usfirst/frc/team1806/robot/auto/actions/ParallelAction.java
@@ -7,28 +7,24 @@ import java.util.List;
  * Composite action, running all sub-actions at the same time All actions are started then updated until all actions
  * report being done.
  *
- * @param A
  *            List of Action objects
  */
 public class ParallelAction implements Action {
-    private boolean[] finishedActions;
+
     private final ArrayList<Action> mActions;
-    private ArrayList<Boolean> mActionStates;
 
     public ParallelAction(List<Action> actions) {
         mActions = new ArrayList<>(actions.size());
-//        for (Action action : actions) {
-//            mActions.add(action);
-//            mActionStates.add(false);
-//        }
+        for (Action action : actions) {
+            mActions.add(action);
+        }
     }
 
     @Override
     public boolean isFinished() {
         boolean all_finished = true;
-        for(int i = 0; i < mActions.size(); i++)
-        {
-            if(!mActionStates.get(i) && !mActions.get(i).isFinished()){
+        for (Action action : mActions) {
+            if (!action.isFinished()) {
                 all_finished = false;
             }
         }
@@ -37,32 +33,15 @@ public class ParallelAction implements Action {
 
     @Override
     public void update() {
-
-        for(int i = 0; i < mActions.size(); i++)
-        {
-
-            if(mActionStates.get(i) == false){
-                Action currentAction = mActions.get(i);
-                if(currentAction.isFinished()){
-                    currentAction.done();
-                    mActionStates.set(i, true);
-                }
-                else{
-                    currentAction.update();
-                }
-            }
-
+        for (Action action : mActions) {
+                action.update();
         }
     }
 
     @Override
     public void done() {
-        for(int i = 0; i < mActions.size(); i++)
-        {
-            if(mActionStates.get(i) == false){
-                Action currentAction = mActions.get(i);
-                currentAction.done();
-            }
+        for (Action action : mActions) {
+            action.done();
         }
     }
 
